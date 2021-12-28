8d56cab06166e1474139df4d69453b5f28b446b6
Dionlan <dionlan.alves@gmail.com>
2021-12-28 13:21:59 -0300

    Atualização do teste de integração com a inserção do arquivo application-test.properties para não utilizar o application da aplicação principal.

--- src/test/java/com/dionlan/minhasfinancas/model/repository/UsuarioServiceRepositoryTest.java
+++ src/test/java/com/dionlan/minhasfinancas/model/repository/UsuarioServiceRepositoryTest.java
@@ -4,17 +4,24 @@ import java.time.LocalDateTime;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.dionlan.minhasfinancas.model.entity.Usuario;
 
 /**
  * Testes de integração. Acessa recursos fora da aplicação, como o repositório do banco de dados. Diferente do teste unitário;
  * @author Dionlan
+ * A partir de out/nov de 2019 a versão 2.2 do Spring Boot implementa o JUnit 5 com as novas anotações
+ * Susbstituir @RunWith(SpringRunner.class) para @ExtendWith(SpringExtension.class)
  *
  */
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("/application-test.properties")
 public class UsuarioServiceRepositoryTest {
 	
 	@Autowired
